e2c68f817da884fce380c6eedc9b637c5c4b7f02
habibi2091 <112811503+habibi2091@users.noreply.github.com>
2022-10-05 18:26:28 +0700

    Create Person.java

---
+++ Pertemuan_5_PPT/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String fname = "Habibi";
+    String lname = "Ardaffenzy";
+    int age = 20;
+
+    public static void main(String[] args) {
+        Person myObj = new Person();
+        System.out.println("Name: "+ myObj.fname + " " + myObj.lname);
+        System.out.println("Age: " + myObj.age);
+    }
+}
