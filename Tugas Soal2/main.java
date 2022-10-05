3df6f13ff640bf1d8d4a9907f5ad5e33e2c47a4b
habibi2091 <112811503+habibi2091@users.noreply.github.com>
2022-10-05 18:40:49 +0700

    Create main.java

---
+++ Tugas Soal2/main.java
@@ -0,0 +1,16 @@
+package LatihanClass3;
+
+public class Main {
+    public static void main(String[] args) {
+        biodata biodata = new biodata();
+        biodata.inputData("2021071031", "Habibi Ardaffenzy");
+        biodata.viewData();
+
+// Yang dibawah ini membuat data Nim dan nama baru.
+
+        biodata mhs = new biodata();
+        mhs.inputData("2021071010","Babon");
+        mhs.viewData();
+
+    }
+}
