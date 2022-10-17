947e0b09e2e62abd4c372aef660e6a2932a130cf
habibi2091 <112811503+habibi2091@users.noreply.github.com>
2022-10-17 23:22:35 +0700

    Update Smartphone.java

--- UTS PBO 2021/No.1/Smartphone.java
+++ UTS PBO 2021/No.1/Smartphone.java
@@ -11,6 +11,9 @@ public class Smartphone {
     private int menit;
     private int perjam;
     private int detik;
+    private int perjamakhir;
+    private int detikakhir;
+    private int menitakhir;
     //membuat set
     public void setJamtelepon(int jam) {
         this.perjam = jam;
@@ -29,25 +32,58 @@ public class Smartphone {
     }
     public int getDetik() {
         return detik;
-    }// ini bakal menghasilkan fungsi pulsa untuk dikembalikan
+    }
+
+    public void setPerjamakhir(int perjamakhir) {
+        this.perjamakhir = perjamakhir;
+    }
+    //Tambahan atribut untuk panggilan akhir
+    public int getPerjamakhir() {
+        return perjamakhir;
+    }
+    public void setMenitanakhir(int menitakhir) {
+        this.menitakhir = menitakhir;
+    }
+    public int getMenitanakhir() {
+        return menitakhir;
+    }
+    public void setDetikakhir(int detikakhir) {
+        this.detikakhir = detikakhir;
+    }
+    public int getDetikakhir() {
+        return detikakhir;
+    }
     public double pulsa(){
         return (3600*perjam + 60*menit + detik)*30;
     }
-    public Smartphone (int jam, int menit, int detik) {
-        this.perjam = jam; this.menit = menit; this.detik = detik;
+    public Smartphone (int jam, int menit, int detik, int perjamakhir, int menitakhir, int detikakhir ) {
+        this.perjam = jam; this.menit = menit; this.detik = detik; this.perjamakhir = perjamakhir;
+        this.menitakhir = menitakhir; this.detikakhir = detikakhir;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Masukkan lama jam:");
-        int perjam = sc.nextInt(); 
+        int perjam = sc.nextInt();
         System.out.print("Masukkan Lama Per menit:");
-        int menit = sc.nextInt(); 
+        int menit = sc.nextInt();
         System.out.print("Masukkan Lama Per detik:");
         int detik = sc.nextInt();
 
-        Smartphone oclock = new Smartphone(perjam,menit,detik);
-        System.out.println("waktu yang dipakai : "+oclock.getJamtelepon() +":"+oclock.getMenit()+":"+oclock.getDetik());
+        System.out.println("");
+
+        System.out.println("Masukkan Lama jam Akhir");
+        System.out.print("Jam akhir : ");
+        int perjamakhir = sc.nextInt();
+        System.out.print("Menit akhir : ");
+        int menitakhir = sc.nextInt();
+        System.out.print("Detik akhir : ");
+        int detikakhir = sc.nextInt();
+
+        Smartphone oclock = new Smartphone(perjam,menit,detik,perjamakhir,menitakhir,detikakhir);
+        System.out.println("waktu Telpon awal : "+oclock.getJamtelepon() +":"+oclock.getMenit()+":"+oclock.getDetik());
+        System.out.println("waktu Telpon akhir : "+oclock.getPerjamakhir()+":"+oclock.getMenitanakhir()
+                +":" +oclock.getDetikakhir());
         System.out.println("biaya percakapan: Rp. " +oclock.pulsa());
     }
 }
