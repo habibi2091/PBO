57d6953a7ac2ed561db4784e41a314434c1287e5
habibi2091 <112811503+habibi2091@users.noreply.github.com>
2022-10-18 00:09:46 +0700

    Create lingkaran.java

---
+++ UTS PBO 2021/No.4/lingkaran.java
@@ -0,0 +1,33 @@
+import java.lang.Math;
+public class lingkaran {
+    private double r;
+    private double PHI = 3.1415;
+    //seperti biasa kita tambahkan atribut nya
+    // berbasis OOP ya
+
+    public lingkaran(){
+        this.r = 0;
+    }
+    public lingkaran(double jari){
+        this.r = jari;
+    }
+    public void setR(double jari) {
+        this.r = jari;
+    }
+    public double getR() {
+        return this.r;
+    }
+    public double luas() {
+        return this.PHI * Math.pow(this.r,2);
+    }
+    public double keliling(){
+        return (2 * this.PHI * this.r);
+    }
+    public static void main(String[] args){
+        lingkaran link1 = new lingkaran();
+        link1.setR(10);
+        System.out.println("Luas Lingkaran ke-1 = " + link1.luas());
+        lingkaran link2 = new lingkaran(100);
+        System.out.println("Luas Lingkaran ke-2 = " + link2.luas());
+    }
+}
