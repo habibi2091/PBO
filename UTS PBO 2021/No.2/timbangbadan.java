e5628efef2fa8861272184ead6add70c3a106f1c
habibi2091 <112811503+habibi2091@users.noreply.github.com>
2022-10-17 23:46:52 +0700

    Create timbangbadan.java

---
+++ UTS PBO 2021/No.2/timbangbadan.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class timbangbadan {
+    public static double bb, tb;
+    public static double BMI;
+    // disini kita membuat atribut terlebih dahulu
+
+    public static double BMI() {
+        return BMI = bb / (tb/100 * tb /100);
+    }
+// lalu kita mengscan untuk menginput nilai dari sini
+    public static void main(String[] args) {
+        Scanner leng = new Scanner((System.in));
+
+        System.out.println("Masukkan Berat Badan Anda : ");
+        bb = leng.nextDouble();
+
+        System.out.println("Masukkan Tinggi Anda: ");
+        tb = leng.nextDouble(); BMI = BMI();
+        System.out.println();
+
+        if (BMI >= 18.5 && BMI <= 22.9) {
+            System.out.println("Berat Badan kamu bagus ideal");
+        } else if (BMI >= 22.9) {
+            System.out.println("Berat kamu gemuk");
+        } else if (BMI <= 18.5) {
+            System.out.println("Berat Badan kurang");
+        }
+
+    }
+}
